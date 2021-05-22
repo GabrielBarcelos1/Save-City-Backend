a2544f2096b1a2ab5b973fc76525dc62d9412810
gabriel barcelos <gabrielvieirabarcelos02@gmail.com>
2021-05-22 08:27:42 -0300

    bug mobile

--- src/controllers/IncidentControllers.js
+++ src/controllers/IncidentControllers.js
@@ -18,6 +18,8 @@ module.exports = {
         'ongs.ong_city',
         'ongs.ong_district',
         'ongs.whatsapp',])
+        console.log(incidents[0].count = count['count'])
+
         return response.json(incidents)
     },
 async create(request, response){
